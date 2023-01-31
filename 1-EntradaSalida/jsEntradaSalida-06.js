552d0bd6f55958af53076baaee8cfb77a14a4db6
Liyulian <grajedajulianagimena@gmail.com>
2023-01-31 12:26:24 -0300

    Nuevos Cmbios 31/01

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,14 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var numerouno = txtIdNumeroUno.value;
+	var numerodos = txtIdNumeroDos.value;
+	var resultado;
+
+	numerouno = parseInt(numerouno);
+	numerodos = parseInt(numerodos);
+
+	resultado = numerouno + numerodos;
+	alert("El resultado de la suma es: " +resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -3,23 +3,47 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
 function sumar()
 {	
-	alert("ok");	
+	var numerouno = txtIdNumeroUno.value;
+	var numerodos = txtIdNumeroDos.value;
+	var resultado;
+
+	numerouno = parseInt(numerouno);
+	numerodos = parseInt(numerodos);
+
+	resultado = numerouno + numerodos;
+	alert("El resultado de la suma es: " +resultado);	
 }
 
 function restar()
 {
-	
+	var numerouno = txtIdNumeroUno.value;
+	var numerodos = txtIdNumeroDos.value;
+	var resultado;
+
+	numerouno = parseInt(numerouno);
+	numerodos = parseInt(numerodos);
+
+	resultado = (numerouno - numerodos);
+	alert("El resultado de la resta es: "+resultado);
 }
 
 function multiplicar()
 { 
-	
+	var numerouno = txtIdNumeroUno.value;
+	var numerodos = txtIdNumeroDos.value;
+	var resultado;
+
+	numerouno = parseInt(numerouno);
+	numerodos = parseInt(numerodos);
+
+	resultado = numerouno * numerodos;
+	alert("El resultado de la multiplicación es: "+resultado);
 }
 
 function dividir()
 {
 	
 }
-
